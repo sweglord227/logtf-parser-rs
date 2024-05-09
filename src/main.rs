7d6f57058e2888aa60b996bff963a82f7aaa6dc9
sweglord227 <sweglord227@gmail.com>
2024-05-09 13:48:51 -0400

    working parallel log grabbing rate limited to 7 requests per second
    logs need fully serialized + documented

--- src/main.rs
+++ src/main.rs
@@ -4,55 +4,86 @@ use crate::structs::Log;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
+use tokio::time::{sleep, Duration};
+use log::*;
+use futures::StreamExt;
 
-const LOGURL: &str = "https://logs.tf/json/";
+const LOGURL: &str = "https://logs.tf/api/v1/log/";
 
 
 #[tokio::main]
 async fn main() {
+    let _ = simple_logger::SimpleLogger::new().init();
     let mut args: Vec<String> = env::args().collect();
 
     args.remove(0);
+    if args.first().is_none() { error!("give args"); return; };
 
-    // for arg in args {
-    let Some(arg) = args.first() else { println!("give args"); return; };
-    let logs = read_lines(arg);
-    let logs = match logs {
-        Ok(r) => r,
-        Err(e) => panic!("{}", e),
-    };
-    let logs = logs.flatten()
-        .into_iter()
-        .map(|log| {
-            tokio::spawn(async move {
-                return get_log(log);
-            })
-        });
-    // }
+    let mut tasks = vec![vec![]];
+    for arg in args {
+        let lines = match read_lines(&arg) {
+            Ok(r) => r,
+            Err(e) => { warn!("Unable to open file [{arg}]. {e}"); continue; }
+        };
+        let (mut line_number, mut index) = (0, 0);
+        for line in lines {
+            line_number += 1;
+            if line_number == 7 {
+                line_number = 0;
+                index += 1;
+            }
+            if index == tasks.len() { tasks.push(vec![]); }
+            match line {
+                Ok(log_id) => tasks[index].push(log_id),
+                Err(e) => { warn!("{e}"); continue;
+                }
+            }
+        }
+    }
+
+    if tasks.is_empty() { return; }
+
+    let mut logs = vec![vec![]];
+    for task in tasks {
+        let _ = sleep(Duration::from_secs_f64(1.0)).await;
+        logs.push(futures::stream::iter(task.iter())
+            .map(|id| get_log(id.to_string()))
+            .buffer_unordered(task.len())
+            .collect()
+            .await);
+    }
 
     for log in logs {
-        let log: Log = match log.await {
-            Ok(r) => match r.await {
+        for l in &log {
+            let log: &Log = match l {
+                Err(e) => { warn!("{e}"); continue; },
                 Ok(r) => r,
-                Err(e) => panic!("{}", e),
-            },
-            Err(e) => panic!("{}", e),
-        };
+            };
 
-        match log.players.get("[U:1:90026832]") {
-                Some(p) => println!("{}", p.deaths),
-                None => println!("ballin"),
+            // for (name, _player) in log.players.iter() {
+            //     // for class in player.class_stats.iter() {
+            //     //     for weapon in class.weapon.iter() {
+            //     //         // let weapon = weapon.0;
+            //     //         // print!("{weapon}, ");
+            //     //     }
+            //     // }
+            //     print!("{}", name)
+            // }
+            println!("");
+            for message in &log.chat {
+                println!("{} :  {}", message.name, message.message);
+            }
         }
     }
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines(filename: &String) -> io::Result<io::Lines<io::BufReader<File>>> {
+    debug!("Reading file: {filename}");
     let file = File::open(filename)?;
     return Ok(io::BufReader::new(file).lines());
 }
 
 async fn get_log(id: String) -> Result<Log, Box<dyn std::error::Error>> {
+    debug!("Getting log: {id}");
     return Ok(serde_json::from_str(&reqwest::get(LOGURL.to_owned() + &id).await?.text().await?)?);
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,15 +1,22 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Log {
+    /// Log API version
     pub version: u32,
     pub teams: Teams,
+    /// Length of the match in seconds
     pub length: u32,
+    /// SteamID3 -> [Player]
     pub players: HashMap<String, Player>,
+    /// SteamID3 -> User Name
+    pub names: HashMap<String, String>,
+    pub rounds: Vec<Round>,
+    pub chat: Vec<Message>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Teams {
     #[serde(rename="Blue")]
     pub blue: Team, 
@@ -17,26 +24,27 @@ pub struct Teams {
     pub red: Team 
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Team {
     pub score: u32,
     pub kills: u32,
     pub deaths: u32,
-    pub dmg: u32,
+    #[serde(rename="dmg")]
+    pub damage: u32,
     pub charges: u32,
     pub firstcaps: u32,
     pub caps: u32
 }
 
-#[derive(Deserialize)]
+/// **Note:** `winner` is `None` when Tie or Stalemate
+#[derive(Deserialize, Debug)]
 pub struct Round {
     pub start_time: u32,
-    pub winner: String,
+    pub winner: Option<String>,
     pub team: RoundTeams,
-
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Events {
     #[serde(rename="type")]
     pub which: String,
@@ -45,7 +53,7 @@ pub struct Events {
     pub team: String
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct RoundTeams { 
     #[serde(rename="Blue")]
     pub blue: RoundTeam, 
@@ -53,7 +61,7 @@ pub struct RoundTeams {
     pub red: RoundTeam 
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct RoundTeam {
     pub score: u32,
     pub kills: u32,
@@ -61,24 +69,24 @@ pub struct RoundTeam {
     pub ubers: u32
 }
 
-#[derive(Deserialize)]
-pub struct Players {
-    // pub steam_id: HashMap<String, Player>,
-}
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Player {
     pub team: String,
+    pub class_stats: Vec<ClassStats>,
     pub kills: u32,
     pub deaths: u32,
     pub assists: u32,
     pub suicides: u32,
-    pub kapd: String,
-    pub kpd: String,
-    pub dmg: u32,
-    pub dmg_real: u32,
+    #[serde(rename="kapd")]
+    pub kills_assists_per_death: String,
+    #[serde(rename="kpd")]
+    pub kills_per_death: String,
+    #[serde(rename="dmg")]
+    pub damage: u32,
+    #[serde(rename="dmg_real")]
+    pub damage_real_wtf: u32,
     #[serde(rename="dt")]
-    pub dmg_taken: u32,
+    pub damage_taken: u32,
     pub lks: u32,
     #[serde(rename="as")]
     pub airshots: u32,
@@ -88,38 +96,42 @@ pub struct Player {
     pub ubertypes: UberTypes,
     pub drops: u32,
     pub medkits: u32,
-    pub medkits_hp: u32,
+    #[serde(rename="medkits_hp")]
+    pub health_from_medkits: u32,
     pub backstabs: u32,
     pub headshots: u32,
     pub sentries: u32,
     pub heal: u32,
     pub cpc: u32,
     pub ic: u32,
-    // pub medicstats: MedicStats,
+    pub medicstats: Option<MedicStats>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct ClassStats {
     #[serde(rename="type")]
     pub class: String,
     pub kills: u32,
     pub assists: u32,
     pub deaths: u32,
-    pub dmg: u32,
+    #[serde(rename="dmg")]
+    pub damage: u32,
     pub weapon: HashMap<String, Weapon>,
     pub total_time: u32,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Weapon {
     pub kills: u32,
-    pub dmg: u32,
-    pub avg_dmg: u32,
+    #[serde(rename="dmg")]
+    pub damage: u32,
+    #[serde(rename="avg_dmg")]
+    pub average_damage: f32,
     pub shots: u32,
     pub hits: u32,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct UberTypes {
     pub medigun: Option<u32>,       
     pub kritskrieg: Option<u32>,
@@ -127,14 +139,24 @@ pub struct UberTypes {
     pub quickfix: Option<u32>,
 }
 
-#[derive(Deserialize)]
+/// **Note**: All medic stats might be `None` because who the fuck knows.
+#[derive(Deserialize, Debug)]
 pub struct MedicStats {
-    pub advantages_lost: u32,
-    pub biggest_advantage_lost: u32,
-    pub deaths_with_95_99_uber: u32,
-    pub deaths_within_20s_after_uber: u32,
-    pub avg_time_before_healing: u32,
-    pub avg_time_to_build: u32,
-    pub avg_time_before_using: u32,
-    pub avg_uber_length: u32,
+    pub advantages_lost: Option<u32>,
+    pub biggest_advantage_lost: Option<u32>,
+    pub deaths_with_95_99_uber: Option<u32>,
+    pub deaths_within_20s_after_uber: Option<u32>,
+    pub avg_time_before_healing: Option<f32>,
+    pub avg_time_to_build: Option<f32>,
+    pub avg_time_before_using: Option<f32>,
+    pub avg_uber_length: Option<f32>,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Message {
+    pub steamid: String,
+    pub name: String,
+    #[serde(rename="msg")]
+    pub message: String,
 }
